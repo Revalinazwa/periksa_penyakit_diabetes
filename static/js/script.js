faf518897a346884b92b9a89e33c9a610f4a4a7c
ahmadseloabadi <ahmadseloabadi@gmail.com>
2025-05-02 01:49:51 +0700

    feat : enhance index.html

--- static/js/script.js
+++ static/js/script.js
@@ -5,41 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
     once: false,
     mirror: true,
   });
-
-  const mobileMenuButton = document.getElementById("mobile-menu-button");
-  const mobileMenu = document.getElementById("mobile-menu");
-
-  mobileMenuButton.addEventListener("click", function () {
-    mobileMenu.classList.toggle("hidden");
-
-    if (mobileMenuButton.innerHTML.includes("fa-bars")) {
-      mobileMenuButton.innerHTML = '<i class="fas fa-times text-xl"></i>';
-    } else {
-      mobileMenuButton.innerHTML = '<i class="fas fa-bars text-xl"></i>';
-    }
-  });
-
-  const mobileLinks = mobileMenu.querySelectorAll("a");
-  mobileLinks.forEach((link) => {
-    link.addEventListener("click", function () {
-      mobileMenu.classList.add("hidden");
-      mobileMenuButton.innerHTML = '<i class="fas fa-bars text-xl"></i>';
-    });
-  });
-
-  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-    anchor.addEventListener("click", function (e) {
-      e.preventDefault();
-
-      const targetId = this.getAttribute("href");
-      const targetElement = document.querySelector(targetId);
-
-      window.scrollTo({
-        top: targetElement.offsetTop - 80,
-        behavior: "smooth",
-      });
-    });
-  });
 });
 
 window.addEventListener("scroll", function () {
